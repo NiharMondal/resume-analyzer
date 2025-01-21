cff36214684ca94f12a149d4b9fdcff8b5a74c51
Nihar Mondal <developernihar@gmail.com>
2025-01-22 00:54:07 +0600

    add: sign up page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import RegisterForm from "@/components/shared/auth/RegisterForm";
+import AuthPageLeftSide from "@/components/shared/AuthPageLeftSide";
 
 export default function Register() {
-	return <div>Register</div>;
+	return (
+		<section className=" max-w-4xl mx-auto">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-10 place-content-center h-svh">
+				<div className="order-last md:order-first">
+					<AuthPageLeftSide />
+				</div>
+				<RegisterForm />
+			</div>
+		</section>
+	);
 }
